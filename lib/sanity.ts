7c57b69ccd07ba9f6d9b98b3efd6317326b18839
amrsalm300-creator <amrsalm300@gmail.com>
2026-08-19 07:18:56 +0300

    Fix Sanity projectId for Vercel deployment

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'next-sanity';
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'kqicvwbx',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: '2026-07-25',
   useCdn: false,
